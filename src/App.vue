6f753a2896cba1c33f18695f4474c6326fa6cbfc
taeho0304 <tdj03063@gmail.com>
2021-06-29 21:22:51 +0900

    Add deploy workflow

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld msg="Welcome to 배태호 App.js App" />
 </template>
 
 <script>
